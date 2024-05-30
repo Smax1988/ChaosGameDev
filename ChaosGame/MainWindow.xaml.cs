c2a371c5745d5212d338646021e9a8e64505df2e
Smax Desktop <home.maxi.schmidt@gmail.com>
2024-05-30 14:22:16 +0200

    merge test

--- ChaosGame/MainWindow.xaml.cs
+++ ChaosGame/MainWindow.xaml.cs
@@ -11,7 +11,7 @@ using System.Windows.Media.Imaging;
 namespace ChaosGame;
 
 /// <summary>
-/// Interaction logic for MainWindow.xaml TEST
+/// Interaction logic for MainWindow.xaml
 /// </summary>
 public partial class MainWindow : Window
 {
